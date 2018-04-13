23925179ae0fb6a291cbe68ca65e05b47f30dff5
Alejandro <Alejandro@GQ-PD02>
2018-04-13 17:50:59 -0600

    Hola mundo Prueba de repositorio

---
+++ src/temporalalejandro/TemporalAlejandro.java
@@ -0,0 +1,21 @@
+
+package temporalalejandro;
+
+import javax.swing.JOptionPane;
+
+/*
+ * Nombre de la clase:Temporal1
+ * Fecha:13/04/2018 
+ * Version:1.0  
+ * CopyRight:Jose Alejandro Quintanilla
+ * @author Giovanni Tzec
+ */
+public class TemporalAlejandro {
+
+    
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo Prueba de Repositorio)","SIS16-A",1);
+          
+    }
+    
+}
