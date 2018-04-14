a2e6a981e7b879439b2f30eba73db72709f85d6b
Alejandro <Alejandro@GQ-PD02>
2018-04-13 18:04:14 -0600

    Este Aporte Lleva Una segundo JOptionMessague

--- src/temporalalejandro/TemporalAlejandro.java
+++ src/temporalalejandro/TemporalAlejandro.java
@@ -15,6 +15,7 @@ public class TemporalAlejandro {
     
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hola Mundo Prueba de Repositorio)","SIS16-A",1);
+         JOptionPane.showMessageDialog(null, "Muy Buenas Profe.Tzec)","SIS16-A",1);
           
     }
     
